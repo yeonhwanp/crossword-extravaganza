a883c034b17c5a9eea4f1cc8c25f2f0a129058d3
William Luo <wqcluo@mit.edu>
2019-04-30 13:54:25 -0400

    Started the checkConsistency() method for checking file consistency

--- src/crossword/Board.java
+++ src/crossword/Board.java
@@ -1,8 +1,49 @@
 package crossword;
 
+import java.util.List;
 
 public class Board {
+    
+    private final List<Word> words;
 
+    
+    public boolean checkConsistency() {
+        /*
+         * IMPLEMENTATION IDEA
+         * 
+         * Go through every pair of words on the board and check:
+         *    1. If the words have the same orientation (both DOWN or both ACROSS), then they cannot intersect anywhere
+         *          a. Since they have the same orientation, if they DO intersect, they must have the same row or same column
+         *          b. The problem reduces to finding whether or not two 1D ranges overlap (well known trick for doing this), or can just use brute force
+         *    2. If the words have different orientations, they can intersect at most one point
+         *          a. Brute force can be used, or we can find the intersections of the lines, and check if the intersection point lies in both segments
+         *    3. Check to make sure that the words are different (since all words must be unique)
+         */
+        
+        for(int i = 0; i < words.size(); i++) {
+            
+            final Word firstWord = words.get(i);
+            final String firstValue = firstWord.getCorrectValue();
+            final boolean firstVertical = firstWord.isVertical();
+
+            for(int j = i+1; j < words.size(); j++) {
+                
+                final Word secondWord = words.get(j);
+                final String secondValue = secondWord.getCorrectValue();
+                final boolean secondVertical = secondWord.isVertical();
+                
+                if(firstValue.equals(secondValue)) { // #3: every word must be unique
+                    return false;
+                }
+                
+                if(firstVertical == secondVertical) // #1: the words are the same orientation
+                {
+                    // words with the same orientation CANNOT intersect, we check their bounding rectangles
+                    throw new RuntimeException("not done implementing!");
+                }
+            }
+        }
+    }
     
     
 }
--- src/crossword/Word.java
+++ src/crossword/Word.java
@@ -3,4 +3,36 @@ package crossword;
 
 public class Word {
 
+    
+    public String getCorrectValue() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public boolean isVertical() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public boolean isHorizontal() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public int getRowLowerBound() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public int getRowUpperBound() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public int getColumnLowerBound() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public int getColumnUpperBound() {
+        throw new RuntimeException("not yet implemented!");
+    }
+    
+    public int getLength() {
+        throw new RuntimeException("not yet implemented!");
+    }
 }
