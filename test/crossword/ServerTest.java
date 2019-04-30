023f8ae0497ba98dd9cd8c7aa41fd4efa037d0c3
Christopher <ccheung9@mit.edu>
2019-04-30 00:16:42 -0400

    adding in serverTest file

---
+++ test/crossword/ServerTest.java
@@ -0,0 +1,10 @@
+package crossword;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class ServerTest {
+
+}
