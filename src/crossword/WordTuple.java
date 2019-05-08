5b1fa564159b51308654648c98743adcaed7cac3
Christopher <ccheung9@mit.edu>
2019-05-08 02:44:00 -0400

    spec'd WordTuple

--- src/crossword/WordTuple.java
+++ src/crossword/WordTuple.java
@@ -7,13 +7,42 @@ package crossword;
  */
 public class WordTuple {
 
-    private final String word;
+    private final String word; //TODO check restrictions on these guys
     private final String hint;
     private final String direction;
     private final int startRow;
     private final int startCol;
     private final int id;
     
+    
+    /*
+     * Abstraction Function:
+     * AF(word, hint, direction, startRow, startCol, id) = Word tuple that is the word word, with an associated hint hint to
+     * help users guess the word, in the direction direction, starting at row startRow and column startCol, with word ID id.
+     * 
+     * Rep Invariant:
+     *  direction must be ACROSS or DOWN
+     *  startRow and startCol must be non-negative
+     *  id must be positive
+     * 
+     * Safety from rep exposure:
+     *  all of our fields are private and final and immutable, therefore no rep exposure
+     *  none of our fields are saved in any of our methods
+     *  
+     * Thread safety argument:
+     *  We are using an immutable type, WordTuple. Since the fields are all private and final and immutable, there is no
+     *  way to have thread safety threatened.
+     */
+    
+    /**
+     * Create a word tuple
+     * @param pRow row that word starts in
+     * @param pCol column that word starts in
+     * @param pHint hint that to help players guess word
+     * @param pID id of this word in the puzzle
+     * @param pWord actual word that is to be guessed
+     * @param pDirection direction of the word
+     */
     public WordTuple(final int pRow, final int pCol, final String pHint, final int pID, final String pWord, final String pDirection) {
         this.word = pWord;
         this.hint = pHint;
@@ -21,28 +50,62 @@ public class WordTuple {
         this.startRow = pRow;
         this.startCol = pCol;
         this.id = pID;
+        checkRep();
     }
     
+    /**
+     * Check for valid rep
+     */
+    private void checkRep() {
+        assert direction.equals("ACROSS") || direction.equals("DOWN");
+        assert startRow >= 0 && startCol >= 0;
+        assert id > 0;
+    }
+    
+    /**
+     * Get the actual, correct word itself
+     * @return correct word
+     */
     public String getWord() {
         return word;
     }
     
+    /**
+     * Get the associated hint with this word
+     * @return associated int
+     */
     public String getHint() {
         return hint;
     }
     
+    /**
+     * Get the direction that the word goes in
+     * @return direction of the word
+     */
     public String getDirection() {
         return direction;
     }
     
+    /**
+     * Get the row that the word starts in
+     * @return the starting row
+     */
     public int getRow() {
         return startRow;
     }
     
+    /**
+     * Get the column that the word starts in
+     * @return the starting column
+     */
     public int getCol() {
         return startCol;
     }
     
+    /**
+     * Get the numeric ID of this word
+     * @return numeric ID of the word
+     */
     public int getID() {
         return id;
     }
