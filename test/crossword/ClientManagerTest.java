787b6095718e57f58777a6d212c239a5452ba979
William Luo <wqcluo@mit.edu>
2019-05-15 09:38:08 -0400

    Updated toString

--- test/crossword/ClientManagerTest.java
+++ test/crossword/ClientManagerTest.java
@@ -27,6 +27,58 @@ public class ClientManagerTest {
      * Testing Strategy
      * 
      * receiveResponse:
-     * 
+     *  - 0 lines
+     *  - 1 line
+     *  - > 1 line
      */
+    
+    @Test
+    public void testAssertionsEnabled() {
+        assertThrows(AssertionError.class, () -> { assert false; },
+                "make sure assertions are enabled with VM argument '-ea'");
+    }
+    
+    /*
+     * Paritions covered: 0 lines
+     */
+    @Test
+    public void testReceiveResponse0() throws IOException {
+        final String readerText = "";
+        final Reader inputReader = new StringReader(readerText);
+        final BufferedReader bfReader = new BufferedReader(inputReader);
+        
+        final String result = ClientManager.receiveResponse(bfReader);
+        final String expected = "";
+        
+        assertEquals(expected, result);
+    }
+    
+    /*
+     * Paritions covered: 1 lines
+     */
+    @Test
+    public void testReceiveResponse1() throws IOException {
+        final String readerText = "12 Hello! Nice to MEET you...";
+        final Reader inputReader = new StringReader(readerText);
+        final BufferedReader bfReader = new BufferedReader(inputReader);
+        
+        final String result = ClientManager.receiveResponse(bfReader);
+        final String expected = readerText + "\n";
+        
+        assertEquals(expected, result);
+    }
+    /*
+     * Paritions covered: >1 lines
+     */
+    @Test
+    public void testReceiveResponseG1() throws IOException {
+        final String readerText = "12 Hello! Nice to MEET you...\n wow PLEASE give us an A   \n  THIS is great??";
+        final Reader inputReader = new StringReader(readerText);
+        final BufferedReader bfReader = new BufferedReader(inputReader);
+        
+        final String result = ClientManager.receiveResponse(bfReader);
+        final String expected = readerText + "\n";
+        
+        assertEquals(expected, result);
+    }
 }
