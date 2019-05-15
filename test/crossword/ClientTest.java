ca984c6d8d03d2ab9241163020cd0a650cc7b6e5
YeonHwan <parky@mit.edu>
2019-05-15 10:42:50 -0400

    removed unecessary things

--- test/crossword/ClientTest.java
+++ test/crossword/ClientTest.java
@@ -1,21 +1,8 @@
 package crossword;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.SwingUtilities;
 
 import org.junit.jupiter.api.Test;
 
